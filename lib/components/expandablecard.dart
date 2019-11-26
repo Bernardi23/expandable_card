72bde93305c544e331e0408e010b04c9e84fc4de
randomdude583 <randomdude@wetpaintdesigns.com>
2019-11-26 17:55:38 -0600

    Update expandablecard.dart

--- lib/components/expandablecard.dart
+++ lib/components/expandablecard.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class ExpandableCard extends StatefulWidget {
+  /// A bottom card widget that can expand on user drag
+  ///
+  /// `content` attribute must not be null
+
   ExpandableCard({
     @required this.children,
     this.padding = const EdgeInsets.only(top: 5, left: 20, right: 20),
@@ -11,16 +15,43 @@ class ExpandableCard extends StatefulWidget {
     this.backgroundColor = Colors.blueGrey,
     this.hasRoundedCorners = false,
     this.hasHandle = true,
+    this.handleColor = Colors.white,
+    this.isExpandedCallback,
+    this.closeCardCallback,
   });
 
+  /// List of widgets that make the content of the card
   final List<Widget> children;
+
+  /// Padding for the content inside the card
   final EdgeInsetsGeometry padding;
+
+  /// Initial height of the card when it is not expanded
   final double minHeight;
+
+  /// Height of the card when it's fully expanded
   final double maxHeight;
-  final bool hasHandle;
+
+  /// Determines whether the card has shadow or not
   final bool hasShadow;
-  final bool hasRoundedCorners;
+
+  /// Color of the card
   final Color backgroundColor;
+
+  /// Determines whether the card has rounded corners or not
+  final bool hasRoundedCorners;
+
+  /// Determines whether the card has a handle icon or not
+  final bool hasHandle;
+
+  /// Determines the handle color
+  final Color handleColor;
+
+  /// Callback for isExpanded;
+  final Function isExpandedCallback;
+
+  /// Callback for closing card;
+  final Function closeCardCallback;
 
   @override
   _ExpandableCardState createState() => _ExpandableCardState();
@@ -42,10 +73,6 @@ class _ExpandableCardState extends State<ExpandableCard>
         AnimationController(vsync: this, duration: Duration(milliseconds: 300));
   }
 
-  final Widget positionDebugContent = Column(
-    children: <Widget>[Text("Hello world"), SizedBox(height: 50), Text("hi")],
-  );
-
   void _startCardDrag(DragStartDetails details) {
     setState(() {
       _isAnimating = false;
@@ -64,6 +91,11 @@ class _ExpandableCardState extends State<ExpandableCard>
         _scrollPercent -= drag / 500;
       });
     }
+    else if (widget.closeCardCallback != null &&(drag > 0.3 && !_cardIsExpanded)) {
+      setState(() {
+        _scrollPercent -= drag / 500;
+      });
+    }
   }
 
   void _endCardDrag(DragEndDetails details) {
@@ -73,44 +105,68 @@ class _ExpandableCardState extends State<ExpandableCard>
         (details.primaryVelocity < -500 || _scrollPercent > 0.6)) {
       _animationScrollPercent =
           Tween<double>(begin: _scrollPercent, end: 1.0).animate(
-        CurvedAnimation(parent: _animationController, curve: _bounceOutCurve),
-      );
+            CurvedAnimation(
+                parent: _animationController, curve: _bounceOutCurve),
+          );
       _animationController.forward();
       setState(() {
         _scrollPercent = 1.0;
         _cardIsExpanded = true;
+        widget.isExpandedCallback(_cardIsExpanded);
       });
+
     } else if (_cardIsExpanded &&
         (details.primaryVelocity > 200 || _scrollPercent < 0.6)) {
       _animationScrollPercent =
           Tween<double>(begin: _scrollPercent, end: 0.0).animate(
-        CurvedAnimation(parent: _animationController, curve: _bounceOutCurve),
-      );
+            CurvedAnimation(
+                parent: _animationController, curve: _bounceOutCurve),
+          );
       _animationController.forward();
       setState(() {
         _scrollPercent = 0.0;
         _cardIsExpanded = false;
+        widget.isExpandedCallback(_cardIsExpanded);
       });
-    }
-    // Card Slider will not expand
-    else {
+
+    } else if (widget.closeCardCallback != null && (!_cardIsExpanded && details.primaryVelocity > 200 || _scrollPercent < -0.15)) {
+      _animationScrollPercent =
+          Tween<double>(begin: _scrollPercent, end: -0.5).animate(
+            CurvedAnimation(
+                parent: _animationController, curve: _bounceOutCurve),
+          );
+      _animationController.forward();
+      setState(() {
+        _scrollPercent = -0.5;
+        if(widget.closeCardCallback != null){
+          widget.closeCardCallback();
+        }
+        _isAnimating = false;
+        _scrollPercent = 0;
+      });
+      _animationController.reset();
+
+
+      // Card Slider will not expand
+    } else {
       if (_cardIsExpanded) {
         _animationScrollPercent =
             Tween<double>(begin: _scrollPercent, end: 1.0).animate(
-          CurvedAnimation(parent: _animationController, curve: _bounceOutCurve),
-        );
+              CurvedAnimation(parent: _animationController, curve: _bounceOutCurve),
+            );
         _animationController.forward();
         setState(() => _scrollPercent = 1.0);
       } else {
         _animationScrollPercent =
             Tween<double>(begin: _scrollPercent, end: 0.0).animate(
-          CurvedAnimation(parent: _animationController, curve: _bounceOutCurve),
-        );
+              CurvedAnimation(parent: _animationController, curve: _bounceOutCurve),
+            );
         _animationController.forward();
         setState(() => _scrollPercent = 0.0);
       }
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -118,7 +174,7 @@ class _ExpandableCardState extends State<ExpandableCard>
       animation: _animationController,
       builder: (context, child) {
         double factor =
-            _isAnimating ? _animationScrollPercent.value : _scrollPercent;
+        _isAnimating ? _animationScrollPercent.value : _scrollPercent;
         double top = MediaQuery.of(context).size.height -
             widget.minHeight -
             (widget.maxHeight - widget.minHeight) * factor;
@@ -135,9 +191,9 @@ class _ExpandableCardState extends State<ExpandableCard>
                 color: widget.backgroundColor,
                 borderRadius: widget.hasRoundedCorners
                     ? BorderRadius.only(
-                        topLeft: Radius.circular(15.0),
-                        topRight: Radius.circular(15.0),
-                      )
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                )
                     : null,
                 boxShadow: [
                   if (widget.hasShadow)
@@ -152,11 +208,16 @@ class _ExpandableCardState extends State<ExpandableCard>
                 padding: widget.padding,
                 child: Column(
                   children: <Widget>[
-                    if (widget.hasHandle) Handle(),
-                    SizedBox(height: 10),
+                    if (widget.hasHandle) Icon(
+                      Icons.remove,
+                      color: widget.handleColor,
+                      size: 45,
+                    ),
+                    SizedBox(height: 5),
                     ...widget.children
                   ],
                 ),
+                // child: positionDebugContent,
               ),
             ),
           ),
@@ -172,13 +233,3 @@ class _ExpandableCardState extends State<ExpandableCard>
   }
 }
 
-class Handle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Icon(
-      Icons.remove,
-      color: Colors.white30,
-      size: 45,
-    );
-  }
-}
